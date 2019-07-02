c207743a1ad78c927ea4aef51168d158854fccf0
Jan <jan@jluehr.de>
2019-07-02 10:07:31 +0200

    Initial import

---
+++ Makefile
@@ -0,0 +1,56 @@
+# GLUON-RELEASE to use
+# Hardcoded for addtional packages  - see below
+#ifndef GLUON_RELEASE
+#	GLUON_RELEASE:=v2018.2
+#endif
+
+#What targets to use?
+ifndef TARGETS
+	TARGETS:=ar71xx-generic
+endif
+
+
+###
+### Nothing to be changed from here
+###
+
+world: dist
+
+# Create all distribution artifacts in ./dist
+dist: $(addprefix dist/,$(TARGETS))
+	mv $(PWD)/gluon/output/packages $(PWD)/dist
+	find $(PWD)/dist -type f -print0  | xargs -0 sha512sum > $(PWD)/dist/sha512sums
+
+# Log Build
+init: gluon/Makefile
+	mkdir -p  $(PWD)/dist
+	date > $(PWD)/dist/log.txt
+	@echo "GLUON_RELEASE: $(GLUON_RELEASE)" >> $(PWD)/dist/log.txt
+	@echo "REPOSITORY_PREFIX: $(REPOSITORY_PREFIX)" >> $(PWD)/dist/log.txt
+	@echo "SITE_BRANCH: $(SITE_BRANCH)" >> $(PWD)/dist/log.txt
+	@echo "Gluon git status" >> $(PWD)/dist/log.txt
+	cd gluon; git branch -v >> $(PWD)/dist/log.txt
+
+.PHONY: dist/% init clean clean-% dist world
+# Create a distribution for a certain gluon
+dist/%: init
+	cp -a site/* $(PWD)/gluon/site/
+	envsubst < site/site.conf > $(PWD)/gluon/site/site.conf
+	make -C gluon update
+
+	echo "Building Target: $*" >> $(PWD)/dist/out.txt
+	echo "Building Target: $*" >> $(PWD)/dist/err.txt
+
+	mkdir -p $(PWD)/dist/
+	make -j2 -C gluon all GLUON_TARGET=$* V=99 2>> $(PWD)/dist/err.txt >> $(PWD)/dist/out.txt
+	rsync -Hav $(PWD)/gluon/output/images/ $(PWD)/dist/
+
+	make -j2 -C gluon clean GLUON_TARGET=$*
+
+gluon/Makefile:
+	#git clone https://github.com/freifunk-gluon/gluon.git -b $(GLUON_RELEASE)
+	git clone https://github.com/yanosz/gluon.git -b master+wifi-encrypt
+	mkdir -p $(PWD)/gluon/site/i18n
+
+clean:
+	rm -Rf dist
---
+++ site/site.mk
@@ -0,0 +1,111 @@
+##	GLUON_SITE_PACKAGES
+#		specify gluon/openwrt packages to include here
+#		The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
+
+GLUON_MULTIDOMAIN=1
+
+GLUON_SITE_PACKAGES := \
+        -hostapd-mini \
+				-uhttpd \
+	gluon-mesh-batman-adv-15 \
+	gluon-respondd \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-ebtables-limit-arp \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	haveged \
+	iwinfo \
+	respondd-module-airtime \
+	gluon-authorized-keys \
+	gluon-wlan-encryption-psk
+
+# basic support for USB stack
+USB_PACKAGES_BASIC := \
+	kmod-usb-core \
+	kmod-usb2
+
+# storage support for USB devices
+USB_PACKAGES_STORAGE := \
+	block-mount \
+	blkid \
+	kmod-fs-ext4 \
+	kmod-fs-vfat \
+	kmod-usb-storage \
+	kmod-usb-storage-extras \
+	kmod-nls-cp1250 \
+	kmod-nls-cp1251 \
+	kmod-nls-cp437 \
+	kmod-nls-cp775 \
+	kmod-nls-cp850 \
+	kmod-nls-cp852 \
+	kmod-nls-cp866 \
+	kmod-nls-iso8859-1 \
+	kmod-nls-iso8859-13 \
+	kmod-nls-iso8859-15 \
+	kmod-nls-iso8859-2 \
+	kmod-nls-koi8r \
+	kmod-nls-utf8 \
+	swap-utils
+
+# network support for USB devices
+USB_PACKAGES_NET := \
+	kmod-mii \
+	kmod-nls-base \
+	kmod-usb-net \
+	kmod-usb-net-asix \
+	kmod-usb-net-asix-ax88179 \
+	kmod-usb-net-cdc-eem \
+	kmod-usb-net-cdc-ether \
+	kmod-usb-net-cdc-mbim \
+	kmod-usb-net-cdc-ncm \
+	kmod-usb-net-cdc-subset \
+	kmod-usb-net-dm9601-ether \
+	kmod-usb-net-hso \
+	kmod-usb-net-huawei-cdc-ncm \
+	kmod-usb-net-ipheth \
+	kmod-usb-net-kalmia \
+	kmod-usb-net-kaweth \
+	kmod-usb-net-mcs7830 \
+	kmod-usb-net-pegasus \
+	kmod-usb-net-qmi-wwan \
+	kmod-usb-net-rndis \
+	kmod-usb-net-sierrawireless \
+	kmod-usb-net-smsc95xx
+
+TOOLS_PACKAGES := \
+	iperf \
+	socat \
+	tcpdump \
+	usbutils \
+	vnstat
+
+GLUON_SITE_PACKAGES += \
+	$(USB_PACKAGES_BASIC) \
+	$(USB_PACKAGES_STORAGE) \
+	$(USB_PACKAGES_NET) \
+	$(TOOLS_PACKAGES)
+
+# Firmware name pattern
+DEFAULT_GLUON_RELEASE := dev
+
+##	GLUON_RELEASE
+#		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
+#		e.g.:
+#			$ make images GLUON_RELEASE=23.42+5
+#		would generate images named like this:
+#			gluon-ff%site_code%-23.42+5-%router_model%.bin
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+# Default priority for updates.
+GLUON_PRIORITY ?= 0
+
+# Languages to include
+GLUON_LANGS ?= en de
+
+# Region settings for ARCHERC7
+GLUON_REGION ?= eu
+
+GLUON_WLAN_MESH = 11s
